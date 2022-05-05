a5525436f43ac9da313cbef3a79e014772950601
Marcel Ochsendorf <info@prodevmo.com>
2022-05-05 14:47:43 +0200

    Update start_can2vesc.sh

--- src/start_can2vesc.sh
+++ src/start_can2vesc.sh
@@ -1,3 +1,21 @@
 #!/bin/env bash
 cd "$(dirname "$0")"
+
+
+# ENBALE KERNEL MODULES FOR CAN
+modprobe can
+modprobe can-raw
+modprobe slcan
+
+ifconfig can0 down  || true
+
+
+# SETUP CAN INTERFACE
+# -s SPEED -s5 => 250k
+slcan_attach -f -n can0 -s5 -o /dev/ttyCAN
+slcand ttyCAN can0
+ifconfig can0 up
+
+
+
 python3 main.py
